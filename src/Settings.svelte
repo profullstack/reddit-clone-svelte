18461955972f35c309f70d9d0897185c835869b1
Samuel-Chris <46243345+Samuel-Chris@users.noreply.github.com>
2020-04-04 01:01:49 +0100

    Single save button (#56)
    * Update settings page
    
    * Removed log statements, reload on submit, fixed links bug
    
    * Added delete option
    
    * Single save button
    
    * Update settings page
    
    * Removed log statements, reload on submit, fixed links bug
    
    * Added delete option
    
    * Single save button
    
    * fix merge conflicts
    
    * linting

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -22,7 +22,7 @@ const newLink = (event) => {
 
 const deleteLink = (e, i) => {
   e.preventDefault();
-   if (user.links.length > 0 && user.links.length < 3) {
+   if (user.links.length > 0 && user.links.length <= 3) {
     if (i > -1) {
       user.links.splice(i, 1);
       user.links = user.links;
@@ -51,7 +51,6 @@ const updateBT = async (event) => {
 
   if (!res.ok) alert('Something went wrong!')
 
-  return location.reload();
 }
 
 const updateLinks = async (event) => {
@@ -63,11 +62,10 @@ const updateLinks = async (event) => {
   let links = []
 
   for (i=0; i < Math.max(numLinks, user.links.length); i++) {
-    if (formData.get(`link-name${i}`) != "" && formData.get(`link-url${i}`) != "") {
+    if (formData.get(`link-name${i}`) != "" && formData.get(`link-url${i}`) != "" && formData.get(`link-name${i}`) != null && formData.get(`link-url${i}`) != null) {
       links.push({name: formData.get(`link-name${i}`), url: formData.get(`link-url${i}`)})
     }
   }
-  console.log(numLinks, user.links.length)
   const url = 'API_BASE_URL/me/links'
   const res = await fetch(url, {
     method: 'POST',
@@ -81,7 +79,6 @@ const updateLinks = async (event) => {
   })
 
   if (!res.ok) alert('Something went wrong!')
-  return location.reload();
 }
 
   const fetchMe = async () => {
@@ -108,15 +105,9 @@ const updateLinks = async (event) => {
   <fieldset>
     <legend>Bitcoin Address</legend>
     {#if user.bitcoinAddress != null}
-      <input type="text" placeholder="Bitcoin Address" id="btAddress" name="bitcoinAddress" value={user.bitcoinAddress} disabled={!isEditingFieldBT}> 
-      {#if !isEditingFieldBT}
-        <button class="btn button-primary float-right" on:click={(e) => {e.preventDefault(); isEditingFieldBT = !isEditingFieldBT}}>Edit</button>
-      {:else}
-        <button class="button-primary float-right" type="submit" on:click={ updateBT }>Submit</button>
-      {/if}
+      <input type="text" placeholder="Bitcoin Address" id="btAddress" name="bitcoinAddress" value={user.bitcoinAddress}> 
     {:else}
       <input type="text" placeholder="Bitcoin Address" id="btAddress" name="bitcoinAddress">
-      <button class="button-primary float-right" type="submit" on:click={ updateBT }>Submit</button>
     {/if}
   </fieldset>
 </form>
@@ -129,16 +120,11 @@ const updateLinks = async (event) => {
       <legend>Social Links</legend>
       {#each user.links as link} 
         <span>Link {user.links.indexOf(link) + 1}</span>
-        {#if isEditingFieldLinks} <a href="#" class="float-right" on:click={(e) => deleteLink(e, user.links.indexOf(link))}>Delete</a> {/if}
-        <input type="text" placeholder="Name" name={`link-name${user.links.indexOf(link)}`} value={link.name} disabled={!isEditingFieldLinks}>
-        <input type="text" placeholder="Url" name={`link-url${user.links.indexOf(link)}`} value={link.url} disabled={!isEditingFieldLinks}>
+        <a href="#" class="float-right" on:click={(e) => deleteLink(e, user.links.indexOf(link))}>Delete</a>
+        <input type="text" placeholder="Name" name={`link-name${user.links.indexOf(link)}`} value={link.name}>
+        <input type="text" placeholder="Url" name={`link-url${user.links.indexOf(link)}`} value={link.url}>
       {/each}
-      {#if !isEditingFieldLinks}
-        <button class="button-primary float-right" type="submit" on:click={(e) => {e.preventDefault(); isEditingFieldLinks = !isEditingFieldLinks}}>Edit</button>
-      {:else}
-        <button class="button-primary" on:click={newLink}>New Link</button>
-        <button class="button-primary float-right" type="submit" on:click={updateLinks}>Submit</button>
-      {/if}
+        <button class="button-primary float-right" on:click={newLink}>New Link</button>
     </fieldset>
 {:else}
     <fieldset>
@@ -148,12 +134,11 @@ const updateLinks = async (event) => {
         <input type="text" placeholder="Url" name={`link-url${i}`}> 
       {/each}
       <button class="button-primary" on:click={newLink}>New Link</button>
-      <button class="button-primary float-right" type="submit" on:click={updateLinks}>Submit</button>
     </fieldset>
     {/if}
 </form>
 
+<button class="button-primary" type="submit" on:click={(e) => {updateLinks(e); updateBT(e); return location.reload();}}>Save</button>
 
 {/if}
-
 </div>
