c60b7d880a91cb34e1f441710dff1c503614a5cc
lnappsdev <60443647+lnappsdev@users.noreply.github.com>
2020-04-04 17:48:49 +0100

    add subscriber count to sidebar (#58)

--- src/utils/abbreviateNumber.js
+++ src/utils/abbreviateNumber.js
@@ -3,12 +3,13 @@ function abbreviateNumber(value) {
     const suffixes = ["", "K", "M", "B","T"];
     let suffixNum = 0;
    
-   while (newValue >= 1000) {
+    while (newValue >= 1000) {
       newValue /= 1000;
       suffixNum++;
-    }
+    }    
 
-    newValue = newValue.toPrecision(3);
+    if (value < 100) newValue = Math.round(newValue);
+    else newValue = newValue.toPrecision(3);
 
     newValue += suffixes[suffixNum];
     return newValue;
